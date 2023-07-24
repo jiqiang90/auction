7f95b7e57e537eee07635f667c24747fd1038965
JQQQ <jiqiang90@hotmail.com>
2023-07-24 14:05:42 +1200

    more

--- .github/workflows/scripts/benchmark/benchmarking.sh
+++ .github/workflows/scripts/benchmark/benchmarking.sh
@@ -11,16 +11,18 @@ input_disableHistorical=$6
 input_others=$7
 
 # Clean docker container history
-output_dir="/app/output/benchmark/"
+output_dir="output/benchmark/"
 . cleanHistory.sh $output_dir
 
 # Start the Node.js app in the background and save its PID
 #subql-node -f ipfs://$input_deployment --network-endpoint=$input_endpoint --batch-size=$input_batch_size --workers=$input_workers --disable-historical=$input_disableHistorical $input_others --ipfs='https://unauthipfs.subquery.network/ipfs/api/v0' --db-schema=app > /app/output/benchmark/indexing.log 2>&1 &
 
-subql-node -f ipfs://$input_deployment --network-endpoint=$input_endpoint --debug --ipfs='https://unauthipfs.subquery.network/ipfs/api/v0' --db-schema=app > /app/output/benchmark/indexing.log 2>&1 &
+subql-node -f ipfs://$input_deployment --network-endpoint=$input_endpoint --debug --ipfs='https://unauthipfs.subquery.network/ipfs/api/v0' --db-schema=app > output/benchmark/indexing.log 2>&1 &
 
 APP_PID=$!
 
+echo "Path from Benchmarking"
+ls "${PWD}/${output_dir}"
 
 echo "Benchmarking timeout: $input_duration"
 # Wait for timeout
